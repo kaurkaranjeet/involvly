7d500121a800ef50809f53190e06f96ccf4aa659
bharti lakhotra <bharti.lakhotra@digimantra.com>
2020-10-22 18:23:24 +0530

    - super admin menu SIDE

--- resources/js/src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
+++ resources/js/src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
@@ -134,6 +134,7 @@ export default {
     parent:           { type: String },
     reduceNotRebound: { type: Boolean, default: true },
     navMenuItems:     { type: Array,   required: true },
+    adminNavMenuItems:     { type: Array,   required: true },
     title:            { type: String }
   },
   data: () => ({
@@ -168,6 +169,7 @@ export default {
     },
     menuItemsUpdated () {
       if (localStorage.getItem("role_id") == '1') {
+      alert("asdad"); 
       const clone = this.adminNavMenuItems.slice()
 
       for (const [index, item] of this.adminNavMenuItems.entries()) {
@@ -181,6 +183,7 @@ export default {
 
       return clone
       } else {
+      alert("bharti"); 
       const clone = this.navMenuItems.slice()
 
       for (const [index, item] of this.navMenuItems.entries()) {
