cdc170bec01b5ef06d9c46f34f4f53be98ed9135
unknown <harpreet.kaur@digimantra.com>
2020-10-27 19:13:03 +0530

    update

--- app/Http/Controllers/Api/V1/StudentController.php
+++ app/Http/Controllers/Api/V1/StudentController.php
@@ -120,9 +120,9 @@ class StudentController extends Controller {
                     //send notification
                     if (!empty($usersData->device_token) && $usersData->device_token != null) {
                         if (!empty($class)) {
-                            $message = 'Your Children has been joined a new class - ' . $class->class_name;
+                            $message = 'Your Children has joined a new class - ' . $class->class_name;
                         } else {
-                            $message = 'Your Children has been joined a new class';
+                            $message = 'Your Children has joined a new class';
                         }
                         $notify_type = 'JOINEDCLASS';
                         NotificationController::SendNotification($usersData->device_token, $message, $notify_type);
@@ -156,9 +156,9 @@ class StudentController extends Controller {
                     //send notification
                     if (!empty($usersData->device_token) && $usersData->device_token != null) {
                         if (!empty($class)) {
-                            $message = 'Your Children has been leaved class - ' . $class->class_name;
+                            $message = 'Your Children has left class - ' . $class->class_name;
                         } else {
-                            $message = 'Your Children has been leaved class';
+                            $message = 'Your Children has left class';
                         }
                         $notify_type = 'JOINEDCLASS';
                         NotificationController::SendNotification($usersData->device_token, $message, $notify_type);
