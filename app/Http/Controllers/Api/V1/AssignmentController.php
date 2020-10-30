27118fc23f92f6adc57bf9ca9c6e62cead1a0011
bharti lakhotra <bharti.lakhotra@digimantra.com>
2020-10-30 13:42:33 +0530

    - add teacher and subjects

--- app/Http/Controllers/Api/V1/AssignmentController.php
+++ app/Http/Controllers/Api/V1/AssignmentController.php
@@ -321,7 +321,7 @@ class AssignmentController extends Controller {
         if ($validator->fails()) {
             return response()->json(array('error' => true, 'message' => $validator->errors()->first()), 200);
         } else {
-            $getData = SubmittedAssignments::with('Assignments')->where('student_id', $request->student_id)->where('class_id', $request->class_id)->where('submit_status', '1')->get();
+            $getData = SubmittedAssignments::with('subjects')->with('Assignments.User')->where('student_id', $request->student_id)->where('class_id', $request->class_id)->where('submit_status', '1')->get();
 
             if ($getData) {
                 return response()->json(array('error' => false, 'message' => 'Success', 'data' => $getData), 200);
