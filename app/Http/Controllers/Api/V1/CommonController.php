237bea67eaf9d5d4743d2009326cf702bf12f202
unknown <harpreet.kaur@digimantra.com>
2020-10-30 18:21:29 +0530

    update

--- app/Http/Controllers/Api/V1/CommonController.php
+++ app/Http/Controllers/Api/V1/CommonController.php
@@ -91,7 +91,7 @@ class CommonController extends Controller {
         ) THEN TRUE
         ELSE FALSE END)
         AS already_join  , class_code_subject.*, assigned_teachers.teacher_id, users.name")))->with('subjects')
-                                ->leftJoin('assigned_teachers', 'class_code_subject.id', '=', 'assigned_teachers.subject_id')
+                                ->leftJoin('assigned_teachers', 'class_code_subject.subject_id', '=', 'assigned_teachers.subject_id')
                                 ->leftJoin('users', 'assigned_teachers.teacher_id', '=', 'users.id')
                                 ->where('class_code_id', $request->class_id)->get();
 
