95890619e637fccb5104c27160ecdb78c1787511
unknown <harpreet.kaur@digimantra.com>
2020-12-01 15:02:45 +0530

    updatequeue

--- app/Http/Controllers/Api/V1/GroupController.php
+++ app/Http/Controllers/Api/V1/GroupController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\SendMailable;
 use App\User;
 use App\Models\Group;
+use App\Models\GroupMessage;
 use App\Notification;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Support\Facades\Validator;
@@ -69,15 +70,41 @@ class GroupController extends Controller {
             if ($validator->fails()) {
                 throw new Exception($validator->errors()->first());
             } else {
+              $user=User::find($request->user_id);
+              if($request->group_id=='1'){
+               $users= User::where('role_id',3)->where('join_community',1)->where('status',1)->get();
+               foreach($users as $single){
+                 $groups=new GroupMessage;
+                 $groups->to_user_id=$single->id;
+                 $groups->from_user_id=$request->user_id;
+                 $groups->group_id=$request->group_id;
+                 $groups->message=$request->message;
+                  $groups->is_read=0;
+                 $groups->save();
+               }
+                 $group_data= GroupMessage::where('from_user_id',$user->id)->where('group_id',$request->group_id)->get();
+
+             }
+
+             if($request->group_id=='2'){
+              $users= User::where('city',$user->city)->where('join_community',1)->where('status',1)->get();
+              foreach($users as $single){
+               $groups=new GroupMessage;
+               $groups->to_user_id=$single->id;
+               $groups->from_user_id=$request->user_id;
+               $groups->group_id=$request->group_id;
+               $groups->message=$request->message;
+                $groups->is_read=0;
+               $groups->save();
+             }
+
+            $group_data= GroupMessage::where('from_user_id',$user->id)->where('group_id',$request->group_id)->get();
+
+           }
 
                 
-              $groups=new Group;
-              $groups->to_user_id=$request->to_user_id;
-              $groups->from_user_id=$request->from_user_id;
-              $groups->group_id=$request->group_id;
-              $groups->message=$request->message;
-              $groups->save();
-                 return response()->json(array('error' => false, 'data' => $groups), 200);
+             
+                 return response()->json(array('error' => false, 'data' => $group_data), 200);
                
           }
         } catch (\Exception $e) {
