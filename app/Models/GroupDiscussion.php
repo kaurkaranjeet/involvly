29a04798c241a039b8659808985f540ea1815c78
unknown <harpreet.kaur@digimantra.com>
2020-12-22 16:53:33 +0530

    update

---
+++ app/Models/GroupDiscussion.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class GroupDiscussion extends Model
+{ 
+
+	
+	public function User(){
+     return $this->belongsTo('App\User','user_id','id');
+   }
+}
