24b32a832700dd8bc1ea4651c9f1280b563cf449
unknown <harpreet.kaur@digimantra.com>
2020-12-29 11:01:48 +0530

    update

--- app/User.php
+++ app/User.php
@@ -81,6 +81,7 @@ class User extends Authenticatable implements JWTSubject {
        }  
        
        $this->role_id =$request->role_id;
+        $this->exit_groups ='';
        $this->state_id =$request->state_id;
        if(isset($request->school_id)){
          $this->school_id =$request->school_id;
