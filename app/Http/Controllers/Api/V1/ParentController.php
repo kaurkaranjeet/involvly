4e06de5ac80fc210186fc27e4cc26e57720149ed
unknown <harpreet.kaur@digimantra.com>
2021-01-07 16:50:12 +0530

    update

--- app/Http/Controllers/Api/V1/ParentController.php
+++ app/Http/Controllers/Api/V1/ParentController.php
@@ -412,7 +412,7 @@ $data_document = [];
             ELSE 0
             END
             )
-            AS is_accept,schedules.*")))->with('User')->where('created_by', $request->user_id)->orWhereIn('assigned_to', $request->user_id)->orderBy('id', 'DESC')->get();
+            AS is_accept,schedules.*")))->with('User')->whereRaw('FIND_IN_SET(assigned_to,'.$request->user_id.')')->orWhere('created_by', $request->user_id)->orderBy('id', 'DESC')->get();
 
 
             return response()->json(array('error' => false, 'message' => 'Record found', 'data' => $tasks), 200);
