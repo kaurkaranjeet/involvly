f606af8a050c233b5baa59866615d5f25ac8cb35
unknown <harpreet.kaur@digimantra.com>
2021-01-27 15:47:07 +0530

    updatestatus

--- app/Http/Controllers/Api/V1/ParentController.php
+++ app/Http/Controllers/Api/V1/ParentController.php
@@ -684,8 +684,8 @@ $data_document = [];
             throw new Exception($validator->errors()->first());
           }  
           else{ 
-            
-           $users=DB::select( DB::raw("SELECt *, (SELECT group_concat(u.name) from parent_childrens
+
+            $users=DB::select( DB::raw("SELECt *, (SELECT group_concat(u.name) as childrens from parent_childrens
             INNER join users as u On u.id= parent_childrens.children_id  where  parent_id=users.id)  as childrens from users where role_id=3 and school_id=".$request->school_id));
            if(!empty($users)){
 
@@ -694,7 +694,7 @@ $data_document = [];
              throw new Exception('No another parents');
            }
 
-           
+
          }
        }
   catch (\Exception $e) {
