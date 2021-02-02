cffab7658fcc3c3d982449926e8beb8df86a181b
Harpreet <harpreet.kaur@digimantra.com>
2021-02-02 18:47:15 +0530

    update

--- app/Http/Controllers/Api/V1/AssignmentController.php
+++ app/Http/Controllers/Api/V1/AssignmentController.php
@@ -168,6 +168,7 @@ class AssignmentController extends Controller {
                          $notificationobj->notification_type='Assignment';
                           $notificationobj->class_id=$request->class_id;
                           $notificationobj->type='school_notification';
+                          $notificationobj->assignment_id=$request->assignment_id;
                          $notificationobj->from_user_id=$getData->Assignments->teacher_id;
                          $notificationobj->save();
                 // Notification::create(['user_id'=>$assignment_assign_to,'notification_message'=>$message,'type'=>'school_notification','notification_type'=> 'Assignment','from_user_id'=>$getData->Assignments->teacher_id]); 
