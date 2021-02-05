4cfd2d9e4b9981b990f33c20268cf7fdad198743
unknown <harpreet.kaur@digimantra.com>
2021-02-05 16:22:00 +0530

    update

--- app/Http/Controllers/Api/V1/ParentController.php
+++ app/Http/Controllers/Api/V1/ParentController.php
@@ -101,7 +101,7 @@ class ParentController extends Controller {
             if($student_obj->role_id !='4'){
               if(empty($student_obj->family_code)){
                $family_code= md5(trim($student_obj->first_name.$request->user_id));
-               $family_code= substr($family_code,6);
+               $family_code= substr($family_code, 6);
                $student_obj->family_code=$family_code;
              }
              
