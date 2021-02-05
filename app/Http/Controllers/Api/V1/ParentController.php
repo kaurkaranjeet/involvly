1d998674e26125bc9c8f248499acd129eca9bce3
unknown <harpreet.kaur@digimantra.com>
2021-02-05 12:26:52 +0530

    update

--- app/Http/Controllers/Api/V1/ParentController.php
+++ app/Http/Controllers/Api/V1/ParentController.php
@@ -63,9 +63,9 @@ class ParentController extends Controller {
                 throw new Exception($validator->errors()->first());
             } else {
                 $student_obj = new User;
-                $addUser = $student_obj->store($input);
-                $token = JWTAuth::fromUser($addUser);
-                $addUser->jwt_token = $token;
+                $addUser = $student_obj->store($request);
+                /*$token = JWTAuth::fromUser($addUser);
+                $addUser->jwt_token = $token;*/
            return response()->json(array('error' => false, 'message' => $e->getMessage(),'data' =>$addUser), 200);
               
             }
