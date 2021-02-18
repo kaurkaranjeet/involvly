876f0ef80d6d6b4af364f4fc43f76acb1c142499
unknown <harpreet.kaur@digimantra.com>
2021-02-18 16:01:34 +0530

    update

--- app/Http/Controllers/Api/V1/ParentController.php
+++ app/Http/Controllers/Api/V1/ParentController.php
@@ -101,6 +101,7 @@ class ParentController extends Controller {
               $student_obj->state_id=$request->state_id;
               $student_obj->city=$request->city_id;
               $student_obj->school_id=$request->school_id;
+              $student_obj->update_detail='1';
                 $student_obj->type_of_schooling=$request->type_of_schooling;
             $student_obj->status=2;
             if($student_obj->role_id !='4'){
