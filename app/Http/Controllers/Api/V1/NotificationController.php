79d9f7f260d417fa7e018cfc90a29836e2a32f60
bharti lakhotra <bharti.lakhotra@digimantra.com>
2021-02-25 19:51:26 +0530

    unread notifications count for all

--- app/Http/Controllers/Api/V1/NotificationController.php
+++ app/Http/Controllers/Api/V1/NotificationController.php
@@ -140,8 +140,8 @@ if(count($notifications)>0){
 	    'user_id' => 'required|exists:users,id',
             'role_type' => 'required|in:all,child,class',
             'type' => 'required_if:role_type, =,all',
-            'children_id' => 'required_if:role_type, =,child',
-            'class_id' => 'required_if:role_type, =,class',
+//            'children_id' => 'required_if:role_type, =,child',
+//            'class_id' => 'required_if:role_type, =,class',
             
 	]);
 	if($validator->fails()){
@@ -149,9 +149,11 @@ if(count($notifications)>0){
 	}
 	else{
             if($request->role_type == 'child'){
-                $notifications=	Notification::where('from_user_id' , $request->children_id)->where('user_id' , $request->user_id)->where('is_read' , '0')->count(); 
+//                $notifications= Notification::where('from_user_id' , $request->children_id)->where('user_id' , $request->user_id)->where('is_read' , '0')->count(); 
+                $notifications=	Notification::where('user_id' , $request->user_id)->where('is_read' , '0')->count(); 
             }else if($request->role_type == 'class'){
-                $notifications=	Notification::where('user_id' , $request->user_id)->where('class_id' , $request->class_id)->where('is_read' , '0')->count(); 
+//                $notifications=	Notification::where('user_id' , $request->user_id)->where('class_id' , $request->class_id)->where('is_read' , '0')->count(); 
+                $notifications=	Notification::where('user_id' , $request->user_id)->where('is_read' , '0')->count(); 
             } else{
                if($request->type=='school_notification'){
 			$notifications=	Notification::where('user_id' , $request->user_id)->where('type' , 'school_notification')->where('is_read' , '0')->count();
