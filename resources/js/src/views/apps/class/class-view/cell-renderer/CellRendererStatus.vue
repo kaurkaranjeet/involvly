191d1be1bb8c7e55833559e9938cd842c25bce85
bharti lakhotra <bharti.lakhotra@digimantra.com>
2021-05-11 23:44:37 +0530

    trello upated

--- resources/js/src/views/apps/class/class-view/cell-renderer/CellRendererStatus.vue
+++ resources/js/src/views/apps/class/class-view/cell-renderer/CellRendererStatus.vue
@@ -1,41 +1,24 @@
 <template>
-    <vs-chip class="ag-grid-cell-chip" :color="chipColor(params.value)">
-      <span>{{ params.value }}</span>
-    </vs-chip>
+    <div>
+     <vs-button @click="assignedTeacherToClass()"> Assign/Re-assign</vs-button>
+                     </div>
+    </div>
 </template>
 
 <script>
+import axios from "@/axios.js";
 export default {
-  name: 'CellRendererStatus',
-  computed: {
-    chipColor () {
-      return (value) => {
-        if (value === 'active') return 'success'
-        else if (value === 'blocked') return 'danger'
-        else if (value === 'deactivated') return 'warning'
-        else return 'primary'
-      }
-    }
+  name: 'CellRendererActions',
+  methods: {
+    assignedTeacherToClass(){
+      var classId = this.$route.params.classId;
+      var subjectId = this.params.data.subject_id;
+           console.log("classId",classId) 
+           console.log("subjectId",subjectId) 
+           this.$router
+                .push(`/apps/class/assign-teacher/` + classId + `/` + subjectId)
+                .catch(() => {});
+        },
   }
 }
 </script>
-
-<style lang="scss" scpoped>
-.ag-grid-cell-chip {
-  &.vs-chip-success {
-    background: rgba(var(--vs-success),.15);
-    color: rgba(var(--vs-success),1) !important;
-    font-weight: 500;
-  }
-  &.vs-chip-warning {
-    background: rgba(var(--vs-warning),.15);
-    color: rgba(var(--vs-warning),1) !important;
-    font-weight: 500;
-  }
-  &.vs-chip-danger {
-    background: rgba(var(--vs-danger),.15);
-    color: rgba(var(--vs-danger),1) !important;
-    font-weight: 500;
-  }
-}
-</style>
