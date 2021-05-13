3fa9ed3b799d57defdb523942ca02175062c00bc
bharti lakhotra <bharti.lakhotra@digimantra.com>
2021-05-13 12:12:24 +0530

    trello update

--- resources/js/src/views/apps/admin/user-list/cell-renderer/CellRendererActionsSchoolAdmins.vue
+++ resources/js/src/views/apps/admin/user-list/cell-renderer/CellRendererActionsSchoolAdmins.vue
@@ -1,7 +1,7 @@
 <template>
     <div :style="{'direction': $vs.rtl ? 'rtl' : 'ltr'}">
        <feather-icon icon="EyeIcon" svgClasses="h-5 w-5 mr-4 hover:text-primary cursor-pointer"  @click="$router.push(url)" />
-      <feather-icon icon="Trash2Icon" svgClasses="h-5 w-5 hover:text-danger cursor-pointer" @click="confirmDeleteRecord" />
+      <!--<feather-icon icon="Trash2Icon" svgClasses="h-5 w-5 hover:text-danger cursor-pointer" @click="confirmDeleteRecord" />-->
     </div>
 </template>
 
